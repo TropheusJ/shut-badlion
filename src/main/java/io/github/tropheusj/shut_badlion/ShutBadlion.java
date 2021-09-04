5833d69b0a6b1f290e062034e2720eca415b9d9a
Tropheus Jay <jverrellijr@icloud.com>
2021-09-04 19:11:56 -0400

    downgrade to 1.16

--- src/main/java/io/github/tropheusj/shut_badlion/ShutBadlion.java
+++
@@ -1,9 +0,0 @@
-package io.github.tropheusj.shut_badlion;
-
-import net.fabricmc.api.ModInitializer;
-
-public class ShutBadlion implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
